ccff6dfe37ec6b54c501747df6312a20f1bc366d
matiru <gemmatiru@gmail.com>
2023-03-24 10:53:49 +0300

    validation-of-signup-using-joi

--- authentication/services/joi-services.js
+++ authentication/services/joi-services.js
@@ -6,4 +6,30 @@ password:Joi.string().min(6).required()
 
 })
 
-module.exports = {loginSchema}
+
+
+const signUpSchema = Joi.object({
+    name: Joi.string()
+        .required(),
+
+    email: Joi.string()
+        .email({ minDomainSegments: 2, tlds: ['*'] })
+        .required(),
+
+    phonenuber: Joi.number()
+        .min(9)
+        .max(12)
+        .required(),
+
+    gender:Joi.string()
+     .required(),
+
+    password: Joi.string()
+        .pattern(new RegExp('^[a-zA-Z1-9]{5,30}'))
+        .required(),
+
+    
+}).with('password');
+
+module.exports = {loginSchema,signUpSchema}
+
