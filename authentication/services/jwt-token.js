d4eeb73cabc32db1884434f2e75699a6e5bb62ea
matiru <gemmatiru@gmail.com>
2023-03-27 22:53:49 +0300

    creating a JWT TOKEN

---
+++ authentication/services/jwt-token.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config()
+
+let secret = process.env.SECRET
+
+module.exports ={
+    createToken:(data) =>{
+        try{
+            let token = jwt.sign(data,secret,{expiresIn:'6hr'});
+            return token
+        } catch (error){
+            return error
+        }
+    },
+    validateToken:(token) =>{
+        try{
+            let data = jwt.verify(token,secret)
+            return data
+        } catch(error){
+            return error
+        }
+    }
+}
